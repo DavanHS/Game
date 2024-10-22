154cee56443417bb984cc08e570b5f3d20de4712
DavanHS <davanhs470@gmail.com>
2024-10-22 18:53:49 +0530

    Update game.js

--- game.js
+++ game.js
@@ -42,7 +42,7 @@ function checkAnswer(currentLevel){
     }
     else{
         console.log("wrong");
-        var wrong = new Audio("./sounds/wrong.mp3")
+        var wrong = new Audio("./wrong.mp3")
         wrong.play();
         $("body").addClass("game-over");
         setTimeout(function(){
@@ -82,7 +82,7 @@ function nextSequence(){
 
 function playSound(name){
 
-    var audio = new Audio("./sounds/"+ name + ".mp3");
+    var audio = new Audio("./"+ name + ".mp3");
     audio.play();
 }
 
